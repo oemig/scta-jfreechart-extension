1bc7c56278235b006dcf6d0860eb7faf03a3f5c5
Christoph Oemig <info@oemig.net>
2013-04-04 22:51:54 +0200

    initial transfer of classes from scta-tracer project

---
+++ src/main/java/net/oemig/scta/jfreechart/SctaToolTipGenerator.java
@@ -0,0 +1,13 @@
+package net.oemig.scta.jfreechart;
+
+import org.jfree.chart.labels.XYToolTipGenerator;
+import org.jfree.data.xy.XYDataset;
+
+public class SctaToolTipGenerator implements XYToolTipGenerator{
+
+	@Override
+	public String generateToolTip(XYDataset dataset, int series, int item) {
+		return "to be filled SctaToolTipGenerator.java";
+	}
+
+}
---
+++ src/main/java/net/oemig/scta/jfreechart/data/SctaDataset.java
@@ -0,0 +1,17 @@
+package net.oemig.scta.jfreechart.data;
+
+import org.jfree.data.xy.XYDataset;
+
+public interface SctaDataset extends XYDataset{
+	
+	public Number getPerformance(int series, int item);
+	
+	public Number getCoordinationErrorRate(int series, int item);
+	
+	public Number getResponseTimeHalfLifeOfAwarenessRatio(int series, int item);
+	
+	public Number getErrorRate(int series, int item);
+	
+	public String getSessionName(int series, int item);
+
+}
